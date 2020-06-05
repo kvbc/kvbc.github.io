247660ed6e6fc9989e218fb31730e42d18154155
KoxuVBC <64214044+kvbc@users.noreply.github.com>
2020-06-05 12:07:11 +0200

    Update checkbox.js

--- Discord_Channel_Name_Creator/scripts/checkbox.js
+++ Discord_Channel_Name_Creator/scripts/checkbox.js
@@ -1,8 +1,18 @@
+var cname_textarea = document.getElementsByClassName("cname-textarea")[0];
+
 function clicked(className) {
     var element = document.getElementsByClassName(className)[0];
-    if(element.checked)
+    if(element.checked) {
         for(var i=1; i<=3; i++)
-            if(i == Number(className.substr(7))) continue;
-            else
-                document.getElementsByClassName(`option-${i}`)[0].checked = false;
+            document.getElementsByClassName(`option-${i}`)[0].checked = false;
+        var value = cname_textarea.value;
+        for(var i=65; i<=122; i++)
+            if(i>90 && i<97) continue;
+            else {
+                var key = String.fromCharCode(i);
+                var re = new RegExp(key,'g');
+                value = value.replace(re,to[element.id](key))
+            }
+        cname_textarea.value = value;
+    }
 }
