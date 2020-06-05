aa60c6cb6b19c49beac5d047a12d33abe4e206cc
KoxuVBC <64214044+kvbc@users.noreply.github.com>
2020-06-05 17:18:29 +0200

    Update convert.js

--- string_to_char_array/scripts/convert.js
+++ string_to_char_array/scripts/convert.js
@@ -4,13 +4,14 @@ var output = document.getElementsByClassName("output")[0];
 
 button.addEventListener("click", function() {
     var final = "[";
-    for(var c in string.value) {
-        var char = string.value[c];
+    var value = string.value;
+    for(var c in value) {
+        var char = String.fromCodePoint(value.codePointAt(c));
         if      (!options.spaces && char==' ') continue;
         if      (options.toLowerCase)          char = char.toLowerCase();
         else if (options.toUpperCase)          char = char.toUpperCase();
         else if (options.toReverseCase)        char = (char == char.toLowerCase() ? char.toUpperCase() : char.toLowerCase());
-        final += `"${char}"${c==string.value.length-1 ? "" : ", "}`;
+        final += `"${char}"${c==value.length-1 ? "" : ", "}`;
     }
     output.value = final + "]";
 })
