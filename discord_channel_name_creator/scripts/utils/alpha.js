370a9073b3f59b0f3f73a6acc38dae237ba8cd69
KoxuVBC <64214044+kvbc@users.noreply.github.com>
2020-06-05 16:25:29 +0200

    Update alpha.js

--- discord_channel_name_creator/scripts/utils/alpha.js
+++ discord_channel_name_creator/scripts/utils/alpha.js
@@ -1,10 +1,11 @@
 function isAlpha(char) {
+    char = toAlpha(char);
     return char.toLowerCase() != char.toUpperCase();
 }
 
 function getFirstAlpha(str) {
     for(var char in str)
-        if(isAlpha(str[char]))
+        if(isAlpha(String.fromCodePoint(str.codePointAt(char))))
             return char;
     return 0;
 }
